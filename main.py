2cbcd05b52c9a0ceffd3617497ae346152b20f77
Yearsflow <yearsflow@126.com>
2022-08-31 01:42:50 +0800

    update main.py

--- main.py
+++ main.py
@@ -281,8 +281,17 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
 
 
 if __name__ == "__main__":
-    with open("config.txt", encoding="utf-8") as f:
-        config = eval(f.read())
+    try:
+        with open("config.txt", encoding="utf-8") as f:
+            config = eval(f.read())
+    except FileNotFoundError:
+        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
+        os.system("pause")
+        sys.exit(1)
+    except SyntaxError:
+        print("推送消息失败，请检查配置文件格式是否正确")
+        os.system("pause")
+        sys.exit(1)
 
     # 获取accessToken
     accessToken = get_access_token()
